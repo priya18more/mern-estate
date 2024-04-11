aaf7253bc038fbb6daa0fc132b564664340921b4
priya18more <priya18more@gmail.com>
2024-04-11 19:35:50 +0530

    sign up functionality added

--- api/index.js
+++ api/index.js
@@ -17,8 +17,8 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 
