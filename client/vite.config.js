4f255f715f8e7c0031c63992631c735d2242debc
priya18more <priya18more@gmail.com>
2024-05-01 20:32:52 +0530

    redux persist

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +10,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: "http://localhost:3000",
+        changeOrigin: true,
         secure: false
       },
     },
